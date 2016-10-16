d1236d5a1ec5a1db606b064d7cdaf6b866939c04
tonplamm <tonplamm@10.10.0.230>
2016-10-16 21:52:17 +0700

    เย้ ตอนนี้สร้างแมพแล้ว bg ก็ใส่ได้แล้ว ขนาดก็ 600*800 กำลังจะทำให้ไม่ทะลุกำแพงครัช

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -3,13 +3,19 @@ package com.mygdx.game;
 public class World {
 	private Bubble bubble;
     private BubbleGame bubbleGame;
+    private Maze maze;
  
     World(BubbleGame bubbleGame) {
         this.bubbleGame = bubbleGame;
         bubble = new Bubble(100,100);
+        maze = new Maze();
     }
  
     Bubble getBubble() {
         return bubble;
+    }
+    
+    Maze getMaze() {
+        return maze;
     }
 }
--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -11,6 +11,8 @@ public class WorldRenderer {
 	private SpriteBatch batch;
 	Texture bubbleImg;
 	private Bubble bubble;
+	private MazeRenderer mazeRenderer;
+	private Texture bg;
 	
 	public WorldRenderer(BubbleGame bubbleGame, World world) {
         this.bubbleGame = bubbleGame;
@@ -18,9 +20,15 @@ public class WorldRenderer {
         this.world = world;
         bubble = world.getBubble();
         bubbleImg = new Texture("right.png");
+        mazeRenderer = new MazeRenderer(bubbleGame.batch, world.getMaze());
+        bg = new Texture("bg1.jpg");
     }
-	
+
 	public void render(float delta) {
+		batch.begin();
+		batch.draw(bg,0,0);
+		batch.end();
+		mazeRenderer.render();
 		batch.begin();
         Vector2 pos = bubble.getPosition();
         batch.draw(bubbleImg, pos.x, pos.y);
